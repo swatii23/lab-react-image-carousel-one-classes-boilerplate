3e7ee672074d7aa5efdc08382524e26a9b8d79f3
Swati Mandal <swati.m_2000@rediffmail.com>
2024-05-09 12:04:13 +0530

    image carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,7 +7,46 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // implement the class below
 class Carousel extends Component {
-  
+    constructor(props) {
+        super(props);
+        this.state = {
+          currentIndex: 0,
+        };
+    }
+    
+    handlePrev = () => {
+        this.setState((prev) => ({
+            currentIndex: prev.currentIndex === 0 ? images.length - 1 : prev.currentIndex - 1,
+        }));
+    };
+
+    handleNext = () => {
+        this.setState((prev) => ({
+            currentIndex: prev.currentIndex === images.length - 1 ? 0 : prev.currentIndex + 1,
+        }));
+    };
+
+    render() {
+        return (
+            <div className="carousel">
+                <div className="images">
+                    {images.map((image, index) => (
+                    <img
+                        key={index}
+                        src={image.img}
+                        className={index === this.state.currentIndex ? "active" : ""}
+                    />
+                    ))}
+                </div>
+                <button className="btn prev-btn" onClick={this.handlePrev}>
+                    <ArrowBackIosIcon />
+                </button>
+                <button className="btn next-btn" onClick={this.handleNext}>
+                    <ArrowForwardIosIcon />
+                </button>
+            </div>
+        );
+    }  
 }
 
 export default Carousel;
